9cbf6c25bd5f7c29ebf0718fd954b36555e74a70
Hinnerk-Ole Kagel <devlarrox@gmail.com>
2026-05-04 19:58:27 +0200

    Add script to remove Microsoft Gaming Overlay

---
+++ remove_ms-gamingoverlay.ps1
@@ -0,0 +1,6 @@
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 0 /f | Out-Null
+reg add "HKCU\System\GameConfigStore" /v GameDVR_Enabled /t REG_DWORD /d 0 /f | Out-Null
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f | Out-Null
+
+Stop-Process -Name "GameBar" -Force -ErrorAction SilentlyContinue
+Stop-Process -Name "GameBarFTServer" -Force -ErrorAction SilentlyContinue
